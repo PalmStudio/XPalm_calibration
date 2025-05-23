e79d8a1ab43599a8111ff7e311f0836ea6edd342
Rémi Vezy <VEZY@users.noreply.github.com>
2025-05-23 17:14:54 +0200

    Update 1-design_of_experiment.R
    Simplify the script

--- 1-code/3-sensitivity_analysis/1-design_of_experiment.R
+++ 1-code/3-sensitivity_analysis/1-design_of_experiment.R
@@ -1,72 +1,22 @@
-library(sensitivity, dplyr, plotly)
+# A script to generate a design of experiment (DoE) for the Morris method
+# using the parameters defined in the xpalm_parameters.csv file.
+
+library(sensitivity)
 
 df_raw_params <- read.csv("2-results/xpalm_parameters.csv", sep = ";")
-
 df_raw_params <- df_raw_params[!is.na(as.numeric(df_raw_params$low_boundary)) & !is.na(as.numeric(df_raw_params$high_boundary)), ]
-
-colnames(df_raw_params) <- c("variable", "value", "low_boundary", "high_boundary")
-
-Pvar <- df_raw_params[, c("variable", "value", "low_boundary", "high_boundary")]
-rownames(Pvar) <- Pvar$variable
-
-parameters <- as.character(Pvar$variable) #parameter list
-
-nFact <- parameters #number of factor
-
-r <- 30 #number of Morris trajectories
-
-binf <- Pvar$low_boundary; names(binf) <- parameters #inf limit
-bsup <- Pvar$high_boundary; names(bsup) <- parameters #sup limit
-
-Q <- 10
-step <- 2
 
 set.seed(1)
 RNGkind(kind = "L'Ecuyer-CMRG")
 
 etude.morris <- morris(
-    model = NULL,
-    factors = nFact,
-    r = r,
-    design = list(type = "oat", levels = Q, grid.jump = step),
-    binf = binf,
-    bsup = bsup,
-    scale = TRUE
+  model = NULL,
+  factors = df_raw_params$variable,
+  r = 30, # number of Morris trajectories
+  design = list(type = "oat", levels = 10, grid.jump = 2),
+  binf = df_raw_params$low_boundary,
+  bsup = df_raw_params$high_boundary,
+  scale = TRUE
 )
 
-planMorris <- etude.morris$X
-
-plot(as.data.frame(planMorris))
-
-visuTraj <- as.data.frame(planMorris) %>%
-    mutate(sim = row_number()) %>%
-    mutate(trajectory = as.factor(sim %/% length(parameters)))
-
-# Pick 3 parameters to visualize (change as needed)
-selected_vars <- parameters[1:3]
-
-plotly::plot_ly(
-  visuTraj,
-  x = ~get(selected_vars[1]),
-  y = ~get(selected_vars[2]),
-  z = ~get(selected_vars[3]),
-  type = "scatter3d",
-  mode = "lines+markers",
-  color = ~trajectory,
-  line = list(width = 4)
-)
-
-### run simulations of the model on the Morris plan (from here)
-outputMorris=planMorris%>%
-  as.data.frame()%>%
-  mutate(y=simple.model(a,b,c,d))
-
-out=tell(etude.morris,y= outputMorris$y)
-print(etude.morris)
-
-resMorris=data.frame(t(out$ee))
-don_Morris=data.frame(parameter=parameters,mu=apply(X=resMorris,MARGIN = 1,mean),mu_star=apply(X=abs(resMorris),MARGIN = 1,mean),sd=apply(X=resMorris,MARGIN = 1,sd))
-
-
-###----graphs
-plot(etude.morris,main=paste('Morris indices'))
+write.csv2(etude.morris$X, "2-results/doe.csv", row.names = FALSE)
