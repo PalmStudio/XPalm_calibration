d3f90d41d88d2d5d3c9e59f80b53c74d4885a39c
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-07-11 15:33:44 +0200

    Update 1-sim_meteo.jl

--- 1-code/2-simulation/1-sim_meteo.jl
+++ 1-code/2-simulation/1-sim_meteo.jl
@@ -1,162 +1,86 @@
 """
-This is XPalm simulation for each sites to plot the considered parameters
+This is XPalm simulation for sites comparison to plot the considered parameters
 """
+using XPalm
+using PlantMeteo
+using PlantSimEngine
+using DataFrames, CSV, YAML, StatsBase, Dates
+using CairoMakie, AlgebraOfGraphics, Statistics
 
-using Revise
-using XPalm, DataFrames, YAML, CSV
-using CairoMakie, AlgebraOfGraphics
-
-#simulation for SMSE
-
-meteo_smse = CSV.read("2-results/meteorology/meteo_smse_cleaned.csv", missingstring=["NA", "NaN"], DataFrame) #Benin
-parameters = YAML.load_file("0-data/xpalm_parameters.yml"; dicttype=Dict{Symbol,Any})
-
-p = XPalm.Palm(parameters=parameters)
-
-sim = xpalm(
-    meteo_smse,
-    DataFrame,
-    vars=Dict(
-        "Scene" => (:lai, :ET0),
-        "Plant" => (:leaf_area, :biomass_bunch_harvested, :plant_age, :biomass_bunch_harvested_cum, :aPPFD, :carbon_assimilation),
-        "Soil" => (:ftsw, :qty_H2O_C2_Roots, :transpiration),
-        "Leaf" => (:biomass,),
-    ),
-    palm=p
+#load long data
+sites_comb = Dict(
+    "SMSE" => CSV.read("2-results/meteorology/meteo_smse_with_nursery.csv", missingstring=["NA", "NaN"], DataFrame),
+    "PRESCO" => CSV.read("2-results/meteorology/meteo_presco_with_nursery.csv", missingstring=["NA", "NaN"], DataFrame),
+    "TOWE" => CSV.read("2-results/meteorology/meteo_towe_with_nursery.csv", missingstring=["NA", "NaN"], DataFrame)
+)
+combined_df = vcat(
+    [hcat(df, DataFrame(site=fill(site, nrow(df)))) for (site, df) in sites_comb]...
 )
 
-sim[!, :date] = meteo_smse.date[sim.timestep]
+CSV.write("2-results/meteorology/combine_meteo.csv", combined_df)
 
-data(filter(:organ => ==("Scene"), sim)) *
-mapping(:date, :ET0) *
-visual(Lines) |> draw
+#simulation all climate with nursery
 
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :qty_H2O_C2_Roots) *
-visual(Lines) |> draw
+meteo = CSV.read("2-results/meteorology/combine_meteo.csv", missingstring=["NA", "NaN"], DataFrame)
+meteos = [Weather(i) for i in groupby(meteo, :site)]
 
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :ftsw) *
-visual(Lines) |> draw
+function symbolize_keys(x)
+    if isa(x, Dict)
+        return Dict(Symbol(k) => symbolize_keys(v) for (k, v) in x)
+    elseif isa(x, Vector)
+        return [symbolize_keys(i) for i in x]
+    else
+        return x
+    end
+end
 
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :transpiration) *
-visual(Lines) |> draw
+begin
+    params_default = symbolize_keys(YAML.load_file("0-data/xpalm_parameters.yml"))
 
+    params_SMSE = copy(params_default)
+    params_SMSE[:plot][:latitude] = 2.93416
+    params_SMSE[:plot][:altitude] = 15.5
 
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :biomass_bunch_harvested_cum) *
-visual(Lines) |> draw
+    params_PRESCO = copy(params_default)
+    params_PRESCO[:plot][:latitude] = 6.137
+    params_PRESCO[:plot][:altitude] = 15.5
 
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :aPPFD) *
-visual(Lines) |> draw
+    params_TOWE = copy(params_default)
+    params_TOWE[:plot][:latitude] = 7.00
+    params_TOWE[:plot][:altitude] = 15.5
 
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :carbon_assimilation) *
-visual(Lines) |> draw
-
-
-###
-#simulation for PRESCO
-
-meteo_presco = CSV.read("2-results/meteorology/meteo_presco_cleaned.csv", missingstring=["NA", "NaN"], DataFrame) #Benin
-parameters = YAML.load_file("0-data/xpalm_parameters.yml"; dicttype=Dict{Symbol,Any})
-
-p = XPalm.Palm(parameters=parameters)
-
-sim = xpalm(
-    meteo_presco,
-    DataFrame,
-    vars=Dict(
-        "Scene" => (:lai, :ET0),
-        "Plant" => (:leaf_area, :biomass_bunch_harvested, :plant_age, :biomass_bunch_harvested_cum, :aPPFD, :carbon_assimilation),
-        "Soil" => (:ftsw, :qty_H2O_C2_Roots, :transpiration),
+    params = Dict("SMSE" => params_SMSE, "PRESCO" => params_PRESCO, "TOWE" => params_TOWE,)
+    out_vars = Dict{String,Any}("Scene" => (:lai, :ET0),
+        "Plant" => (:leaf_area, :biomass_bunch_harvested, :plant_age, :biomass_bunch_harvested_cum, :aPPFD, :carbon_assimilation, :n_bunches_harvested_cum, :n_bunches_harvested, :Rm, :reserve, :yield_gap_oil, :biomass_oil_harvested),
+        "Soil" => (:ftsw, :qty_H2O_C_Roots, :transpiration),
         "Leaf" => (:biomass,),
-    ),
-    palm=p
-)
+        "Phytomer" => (:phytomer_count,),
+        "Female" => (:biomass_bunch_harvested, :plant_age, :biomass, :fruits_number, :nb_fruits_flag),
+        "Male" => (:biomass,),
+        "Internode" => (:biomass,),)
 
-sim[!, :date] = meteo_presco.date[sim.timestep]
+    simulations = DataFrame[]
+    for m in meteos
+        site = m[1].site
+        palm = XPalm.Palm(parameters=params[site])
+        df = xpalm(meteo, DataFrame, vars=out_vars, palm=palm)
+        df = df["Soil"]
+        df[!, "Site"] .= site
+        push!(simulations, df)
+    end
+end
 
-data(filter(:organ => ==("Scene"), sim)) *
-mapping(:date, :ET0) *
-visual(Lines) |> draw
+dfs_all = vcat(simulations...)
 
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :qty_H2O_C2_Roots) *
-visual(Lines) |> draw
+CSV.write("2-results/simulations/sim_comparison_soil.csv", dfs_all)
 
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :ftsw) *
-visual(Lines) |> draw
+#plot the comparison of number of ftsw < 0.3 within the sites in bar plot
+df_ftsw = CSV.read("2-results/simulations/sim_comparison_soil.csv", missingstring=["NA", "NaN"], DataFrame)
+threshold_ftsw = 0.3
+#drop the rows in :ftsw that is 0
+df_ftsw = df_ftsw[df_ftsw.ftsw.>0, :]
+df_valid = df_ftsw[df_ftsw.ftsw.<threshold_ftsw, :]
+df_valid_count = combine(groupby(df_valid, :Site), nrow => :n_ftsw)
 
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :transpiration) *
-visual(Lines) |> draw
-
-
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :biomass_bunch_harvested_cum) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :aPPFD) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :carbon_assimilation) *
-visual(Lines) |> draw
-
-
-### 
-
-#simulation for TOWE
-meteo_towe = CSV.read("2-results/meteorology/meteo_towe_cleaned.csv", missingstring=["NA", "NaN"], DataFrame) #Benin
-parameters = YAML.load_file("0-data/xpalm_parameters.yml"; dicttype=Dict{Symbol,Any})
-
-p = XPalm.Palm(parameters=parameters)
-
-sim = xpalm(
-    meteo_towe,
-    DataFrame,
-    vars=Dict(
-        "Scene" => (:lai, :ET0),
-        "Plant" => (:leaf_area, :biomass_bunch_harvested, :plant_age, :biomass_bunch_harvested_cum, :aPPFD, :carbon_assimilation),
-        "Soil" => (:ftsw, :qty_H2O_C2_Roots, :transpiration),
-        "Leaf" => (:biomass,),
-    ),
-    palm=p
-)
-
-sim[!, :date] = meteo_towe.date[sim.timestep]
-
-data(filter(:organ => ==("Scene"), sim)) *
-mapping(:date, :ET0) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :qty_H2O_C2_Roots) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :ftsw) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Soil"), sim)) *
-mapping(:date, :transpiration) *
-visual(Lines) |> draw
-
-
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :biomass_bunch_harvested_cum) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :aPPFD) *
-visual(Lines) |> draw
-
-data(filter(:organ => ==("Plant"), sim)) *
-mapping(:date, :carbon_assimilation) *
-visual(Lines) |> draw
-
+plt_ftsw = data(df_valid_count) * mapping(:Site, :n_ftsw) * visual(BarPlot)
+draw(plt_ftsw)
