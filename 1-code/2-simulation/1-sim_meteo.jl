7b986eed4c7e8b20b2a48100fb4a51f5ccc94da1
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-07-14 18:29:30 +0200

    Update 1-sim_meteo.jl

--- 1-code/2-simulation/1-sim_meteo.jl
+++ 1-code/2-simulation/1-sim_meteo.jl
@@ -23,30 +23,20 @@ CSV.write("2-results/meteorology/combine_meteo.csv", combined_df)
 meteo = CSV.read("2-results/meteorology/combine_meteo.csv", missingstring=["NA", "NaN"], DataFrame)
 meteos = [Weather(i) for i in groupby(meteo, :site)]
 
-function symbolize_keys(x)
-    if isa(x, Dict)
-        return Dict(Symbol(k) => symbolize_keys(v) for (k, v) in x)
-    elseif isa(x, Vector)
-        return [symbolize_keys(i) for i in x]
-    else
-        return x
-    end
-end
-
 begin
-    params_default = symbolize_keys(YAML.load_file("0-data/xpalm_parameters.yml"))
+    params_default = (YAML.load_file("0-data/xpalm_parameters.yml"))
 
     params_SMSE = copy(params_default)
-    params_SMSE[:plot][:latitude] = 2.93416
-    params_SMSE[:plot][:altitude] = 15.5
+    params_SMSE["plot"]["latitude"] = 2.93416
+    params_SMSE["plot"]["altitude"] = 15.5
 
     params_PRESCO = copy(params_default)
-    params_PRESCO[:plot][:latitude] = 6.137
-    params_PRESCO[:plot][:altitude] = 15.5
+    params_PRESCO["plot"]["latitude"] = 6.137
+    params_PRESCO["plot"]["altitude"] = 15.5
 
     params_TOWE = copy(params_default)
-    params_TOWE[:plot][:latitude] = 7.00
-    params_TOWE[:plot][:altitude] = 15.5
+    params_TOWE["plot"]["latitude"] = 7.00
+    params_TOWE["plot"]["altitude"] = 15.5
 
     params = Dict("SMSE" => params_SMSE, "PRESCO" => params_PRESCO, "TOWE" => params_TOWE,)
     out_vars = Dict{String,Any}("Scene" => (:lai, :ET0),
@@ -64,7 +54,26 @@ begin
         palm = XPalm.Palm(parameters=params[site])
         df = xpalm(m, DataFrame, vars=out_vars, palm=palm)
         df = df["Soil"]
+
+        # Add site name
         df[!, "Site"] .= site
+
+        # Get matching meteorology data for the current site
+        meteo_site = filter(:site => x -> x == site, meteo)
+
+        # Add :date column by matching timestep
+        df[!, :date] = meteo_site.date[df.timestep]
+
+        #add planting date 
+        planting_date = site == "SMSE" ? Date("2011-01-01") :
+                        site == "PRESCO" ? Date("2010-05-01") :
+                        site == "TOWE" ? Date("2012-06-01") : missing
+
+        df[!, :planting_date] .= planting_date
+
+        #add MAP
+        df[!, :MAP] = [(year(d) - year(planting_date)) * 12 + (month(d) - month(planting_date)) for d in df.date]
+
         push!(simulations, df)
     end
 end
@@ -76,14 +85,16 @@ CSV.write("2-results/simulations/sim_comparison_soil.csv", dfs_all)
 
 #plot the comparison of number of ftsw < 0.3 within the sites in bar plot
 df_ftsw = CSV.read("2-results/simulations/sim_comparison_soil.csv", missingstring=["NA", "NaN"], DataFrame)
-data(df_ftsw) * mapping(:timestep, :ftsw, color=:Site) * visual(Lines) |> draw
+data(df_ftsw) * mapping(:MAP, :ftsw, color=:Site) * visual(Lines) |> draw
 
 
 threshold_ftsw = 0.3
-#drop the rows in :ftsw that is 0
-df_ftsw = df_ftsw[df_ftsw.ftsw.>0, :]
+df_ftsw = df_ftsw[df_ftsw.ftsw.>0, :] #drop the rows in :ftsw that is 0
 df_valid = df_ftsw[df_ftsw.ftsw.<threshold_ftsw, :]
-df_valid_count = combine(groupby(df_valid, :Site), nrow => :n_ftsw)
+df_valid_count = combine(groupby(df_valid, [:Site, :MAP]), nrow => :n_ftsw)
 
-plt_ftsw = data(df_valid_count) * mapping(:Site, :n_ftsw) * visual(BarPlot)
+plt_ftsw = data(df_valid_count) *
+           mapping(:MAP, :n_ftsw, color=:Site) *
+           visual(Lines)
 draw(plt_ftsw)
+save("2-results/simulations/n_ftsw.png", plt_ftsw)
