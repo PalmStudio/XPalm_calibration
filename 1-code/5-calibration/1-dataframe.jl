1ff32ac03203c9592dc51bbd72b8c92cae6d5f2f
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-07-18 16:23:45 +0200

    Update 1-dataframe.jl

--- 1-code/5-calibration/1-dataframe.jl
+++ 1-code/5-calibration/1-dataframe.jl
@@ -11,6 +11,11 @@ mes_cum_n_fruit = CSV.read("2-results/calibration/cumulated_n_fruit_mes.csv", Da
 mes_cum_prod = CSV.read("2-results/calibration/cumulated_production_mes.csv", DataFrame, missingstring=["NA", "NaN"])
 mes_cum_stalk_biomass = CSV.read("2-results/calibration/cumulated_stalk_biomass_mes.csv", DataFrame, missingstring=["NA", "NaN"])
 mes_cum_LAI = CSV.read("2-results/calibration/cumulated_LAI_mes.csv", DataFrame, missingstring=["NA", "NaN"])
+mes_cum_rachis_length = CSV.read("2-results/calibration/cumulated_rachis_length_mes.csv", DataFrame, missingstring=["NA", "NaN"])
+mes_avg_leaflet_length = CSV.read("2-results/calibration/avg_leaflet_length_mes.csv", DataFrame, missingstring=["NA", "NaN"])
+mes_cum_n_new_leaf_emitted = CSV.read("2-results/calibration/cum_n_new_leaf_emitted.csv", DataFrame, missingstring=["NA", "NaN"])
+mes_flowering_time_Phytomer = CSV.read("2-results/calibration/time_leaf_flowering_phytomer.csv", DataFrame, missingstring=["NA", "NaN"]) #date is flowering date, MAP is flowering MAP
+mes_flowering_time_IdGen = CSV.read("2-results/calibration/time_leaf_flowering_IdGenotype.csv", DataFrame, missingstring=["NA", "NaN"])
 
 merged_bunch = outerjoin(mes_cum_biomass_oil,
     mes_cum_n_bunch,
@@ -18,5 +23,10 @@ merged_bunch = outerjoin(mes_cum_biomass_oil,
     mes_cum_prod,
     mes_cum_stalk_biomass,
     mes_cum_LAI,
+    mes_cum_rachis_length,
+    mes_avg_leaflet_length,
+    mes_cum_n_new_leaf_emitted,
+    mes_time_between_leaf_flowering,
+    mes_flowering_time_IdGen,
     on=[:TreeId, :Date, :MAP, :Site, :IdGenotype], makeunique=true)
-sort!(merged_bunch, [:Date])
+sort!(merged_bunch, [:Site, :Date])
