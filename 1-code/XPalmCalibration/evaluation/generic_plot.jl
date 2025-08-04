f6f752b150b899d51f37ad8352603d6d7fe1fec8
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-08-04 16:38:29 +0200

    Update generic_plot.jl
    add filter for the column that has no missing to be able to plot the line if in the observation is missing

--- 1-code/XPalmCalibration/evaluation/generic_plot.jl
+++ 1-code/XPalmCalibration/evaluation/generic_plot.jl
@@ -2,6 +2,7 @@ function evaluate_generic(df, variable_name; ylabel=variable_name, xlabel="Month
     df = copy(df)
     rename_variables_names!(df, variable_name)
     df_long = stack(df, Not(:MAP, :Site), variable_name=:type, value_name=:value)
+    filter!(row -> row.type != "observed" || !ismissing(row.value), df_long)
     p = data(df_long) * mapping(:MAP, :value, row=:Site, color=:type) * visual(Lines)
     fig = draw(p; axis=(; xlabel=xlabel, ylabel=ylabel), figure=(; size=(1000, 600)), legend=(; position=:bottom))
 
