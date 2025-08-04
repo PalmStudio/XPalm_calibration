89db7a6704d721ee3673b2307bb73dc399acc286
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-08-04 18:24:49 +0200

    Update 1-manual_calibration_species.jl
    delete the computed vars with generic

--- 1-code/5-calibration/1-manual_calibration_species.jl
+++ 1-code/5-calibration/1-manual_calibration_species.jl
@@ -23,8 +23,8 @@ end
 df_CIGE_species = combine( #here is we dont consider about the genotype thats why mostly we use the mean from all treeId to get the value of each tree
     groupby(df_CIGE, [:Site, :MAP]),
     :Cumulated_n_leaf_emitted => (x -> fn_no_missings(x, mean)) => :cumulated_n_leaf_emitted, #!done
-    :LeafArea => (x -> fn_no_missings(x, mean)) => :Leaf_area_17, #average leaf area at rank 17 (plant -1)
-    :bunch_fresh_mass_total => (x -> fn_no_missings(x, mean)) => :bunch_fresh_biomass, # in kg,
+    :LeafArea => (x -> fn_no_missings(x, mean)) => :Leaf_area_17, #average leaf area at rank 17 (plant -1) #!done
+    :bunch_fresh_mass_total => (x -> fn_no_missings(x, mean)) => :bunch_fresh_biomass, # in kg, #!FFB
     :bunch_fresh_mass_average => (x -> fn_no_missings(x, mean)) => :bunch_fresh_mass_per_bunch, # in kg
     :bunch_dry_mass_total => (x -> fn_no_missings(x, mean)) => :bunch_dry_biomass, # in kg,
     :bunch_dry_mass_per_bunch => (x -> fn_no_missings(x, mean)) => :bunch_dry_mass_per_bunch, # in kg #! done
@@ -102,11 +102,6 @@ dfs_plant_MAP = simulation_map.plant
 dfs_leaf_MAP = simulation_map.leaf
 dfs_female = simulation_map.female
 
-#plot total number of bunch per plant #!done
-df_n_bunch_long = stack(df_plant, [:total_n_bunches_harvested_sim, :total_n_bunches_harvested_obs], variable_name=:type, value_name=:total_n_bunches_harvested)
-p_n_bunch = data(df_n_bunch_long) * mapping(:MAP, :total_n_bunches_harvested, row=:Site, color=:type) * visual(Lines)
-fig_n_bunch = draw(p_n_bunch; axis=(; xlabel="Month after planting", ylabel="Total number of bunch harvested (plant-1 MAP-1)"), figure=(; size=(1000, 600)), legend=(; position=:bottom))
-save("2-results/calibration/XPalm/5.total_n_bunches_harvested (plant-1 MAP-1).png", fig_n_bunch)
 
 #biomass dry fruit in the plant scale #!to recheck with the latest version of xpalm
 df_fruit_dry_long = stack(df_plant, [:total_biomass_dry_fruit_sim, :total_biomass_dry_fruit_obs], variable_name=:type, value_name=:biomass_dry_fruit)
@@ -114,17 +109,6 @@ p_fruit_dry = data(df_fruit_dry_long) * mapping(:MAP, :biomass_dry_fruit, row=:S
 fig_fruit_dry = draw(p_fruit_dry; axis=(; xlabel="Month after planting", ylabel="Total biomass dry fruit (kg plant-1 MAP-1)"), figure=(; size=(1000, 600)), legend=(; position=:bottom))
 save("2-results/calibration/XPalm/4.Total biomass dry fruit (kg plant-1 MAP-1).png", fig_fruit_dry)
 
-#!Leaf scale simulation
-
-df_leaf = innerjoin(dfs_leaf_MAP, df_CIGE_species, on=[:Site, :MAP], makeunique=true, renamecols="_sim" => "_obs")
-
-#plot leaf area
-df_leaf_area_17_long = stack(df_leaf, [:Leaf_area_17_sim, :Leaf_area_17_obs], variable_name=:type, value_name=:Leaf_area_17)
-filter!(row -> !ismissing(row.Leaf_area_17), df_leaf_area_17_long)
-
-p_leaf_area_17 = data(df_leaf_area_17_long) * mapping(:MAP, :Leaf_area_17, row=:Site, color=:type) * visual(Lines)
-fig_leaf_area_17 = draw(p_leaf_area_17; axis=(; xlabel="Month after planting", ylabel="Leaf area at rank 17"), figure=(; size=(1000, 600)), legend=(; position=:bottom))
-save("2-results/calibration/XPalm/2.leaf_area_rank_17.png", fig_leaf_area_17) #!i think its good
 
 #!Female scale simulation
 #average 1 bunch biomass all time (kg) #!good
@@ -141,9 +125,4 @@ p_avg_bunch_mass = data(df_avg_bunch_long) * mapping(:type, :avg_biomass_bunch, 
 fig_avg_bunch_mass = draw(p_avg_bunch; axis=(; ylabel="Average 1 bunch biomass per site (kg)"), figure=(; size=(1000, 600)), legend=(; position=:bottom))
 save("2-results/calibration/XPalm/3.avg_bunch_mass_site.png", fig_avg_bunch_mass)
 
-#bunch biomass
-df_bunch_fresh_biomass = innerjoin(df_female_MAP, df_CIGE_species, on=[:Site, :MAP], makeunique=true, renamecols="_sim" => "_obs")
-df_bunch_fresh_biomass_long = stack(df_bunch_fresh_biomass, [:bunch_fresh_biomass_sim, :bunch_fresh_biomass_obs], variable_name=:type, value_name=:bunch_fresh_biomass)
-p_bunch_fresh_biomass = data(df_bunch_fresh_biomass_long) * mapping(:MAP, :bunch_fresh_biomass, row=:Site, color=:type) * visual(Lines)
-fig_bunch_fresh_biomass = draw(p_bunch_fresh_biomass; axis=(; xlabel="Month after planting", ylabel="Bunch biomass (g)"), figure=(; size=(1000, 600)), legend=(; position=:bottom))
-save("2-results/calibration/XPalm/bunch_mass_per_MAP.png", fig_bunch_fresh_biomass)
+
