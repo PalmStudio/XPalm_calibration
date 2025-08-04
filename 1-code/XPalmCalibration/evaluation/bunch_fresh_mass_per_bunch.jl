7d8ff843e75addb62b38bdd25a54e15b83b140f0
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-08-04 18:26:35 +0200

    generate new vars

---
+++ 1-code/XPalmCalibration/evaluation/bunch_fresh_mass_per_bunch.jl
@@ -0,0 +1,3 @@
+function evaluate_bunch_fresh_mass_per_bunch(df_female)
+    evaluate_generic(df_female, "bunch_fresh_mass_per_bunch"; ylabel="bunch_fresh_mass_per_bunch (bunch-1 MAP-1)", xlabel="Month after planting")
+end
