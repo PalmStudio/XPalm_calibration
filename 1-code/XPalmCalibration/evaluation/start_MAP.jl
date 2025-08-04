c4697a8cf3f133aca305dff4f5d1c5ed248abf84
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-08-04 16:40:42 +0200

    offset the leaf emitted
    start the computation of leaf emitted as 0 in the simuation depending on the first MAP of observation in each site of CIGE data

---
+++ 1-code/XPalmCalibration/evaluation/start_MAP.jl
@@ -0,0 +1,3 @@
+function start_MAP()
+    return Dict("PR" => 35, "SMSE" => 39, "TOWE" => 26)
+end
--- 1-code/XPalmCalibration/simulation/integrate_simulation_by_map.jl
+++ 1-code/XPalmCalibration/simulation/integrate_simulation_by_map.jl
@@ -4,6 +4,11 @@
 
 
 """
+
+CC_Fruit = 0.4857     # Fruit carbon content (gC g-1 dry mass)
+water_content_mesocarp = 0.25  # Water content of the mesocarp
+dry_to_fresh_ratio = 1 / (1 - water_content_mesocarp)  # Based on the mesocarp water content of 0.3
+
 function integrate_simulation_by_map(simulations)
     # Plant scale
     dfs_plant = vcat([s["Plant"] for s in simulations]...)
@@ -11,7 +16,7 @@ function integrate_simulation_by_map(simulations)
 
     dfs_plant_MAP = combine(
         groupby(dfs_plant, [:Site, :MAP]),
-        :leaf_area => last => :Leaf_area, #total leaf area per month #!done
+        :leaf_area => last => :Leaf_area_17, #total leaf area per month #!done
         :biomass_bunch_harvested => sum => :biomass_bunch_harvested_MAP, #total biomass bunch harvested
         :biomass_bunch_harvested_cum => last => :biomass_bunch_harvested_cum, #dynamic cumulated bunch biomass per MAP
         :biomass_fruit_harvested => sum => :biomass_fruit_harvested_MAP, #gr (?)
@@ -20,11 +25,40 @@ function integrate_simulation_by_map(simulations)
         :phytomer_count => last => :phytomer_count, #total number of phytomer per MAP
         :phytomer_count => (x -> x[end] - x[1]) => :diff_phytomer_emmitted, #the difference phytomer emitted between MAP
     )
+    #!compute FFB (bunch_fresh_biomass from biomass_bunch_harvested)
+    dfs_plant_MAP[!, :bunch_fresh_biomass] = (dfs_plant_MAP.biomass_bunch_harvested_MAP .* 1e-3 ./ CC_Fruit) .* dry_to_fresh_ratio #!FFB
+
+    # Compute cumulated_n_leaf_emitted based on the first observation MAP of CIGE each site
+    dfs_plant_MAP[!, :cumulated_n_leaf_emitted] = Vector{Union{Missing,Int64}}(missing, nrow(dfs_plant_MAP))
+    thresholds = start_MAP()
+
+    for subdf in groupby(dfs_plant_MAP, :Site)
+        site = unique(subdf.Site)[1]
+        threshold = thresholds[site]
+
+        sort!(subdf, :MAP)
+        start_count = findfirst(x -> x >= threshold, subdf.MAP)
+
+        if isnothing(start_count)
+            continue  # skip if no MAP ≥ threshold
+        end
+
+        base_value = subdf.phytomer_count[start_count]
+        cumulated = vcat(
+            fill(missing, start_count - 1),
+            subdf.phytomer_count[start_count:end] .- base_value
+        )
+
+        dfs_plant_MAP[dfs_plant_MAP.Site.==site, :cumulated_n_leaf_emitted] .= cumulated
+    end
+
+    #leaf scale
 
     dfs_leaf = vcat([s["Leaf"] for s in simulations]...)
     sort!(dfs_leaf, [:Site, :timestep])
     filter!(x -> x.rank == 17, dfs_leaf)
     dfs_leaf_MAP = combine(groupby(dfs_leaf, [:Site, :MAP]), :leaf_area => last => :Leaf_area_17)
+    filter!(row -> !ismissing(row.Leaf_area_17), dfs_leaf_MAP)
 
 
     dfs_female = vcat([s["Female"] for s in simulations]...)
