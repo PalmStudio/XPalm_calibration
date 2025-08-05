eb2a56fd2dc11cd8af515f616f7d833ff43c1343
lailanhabibah <lailanur.habibah@agrocampus-ouest.fr>
2025-08-05 15:41:58 +0200

    use the median instead of mean for the number of bunch

--- 1-code/5-calibration/1-manual_calibration_species.jl
+++ 1-code/5-calibration/1-manual_calibration_species.jl
@@ -30,12 +30,13 @@ df_CIGE_species = combine( #here is we dont consider about the genotype thats wh
     :bunch_dry_mass_per_bunch => (x -> fn_no_missings(x, mean)) => :bunch_dry_mass_per_bunch, # in kg #! done
     :biomass_fresh_fruit_per_bunch => (x -> fn_no_missings(x, mean)) => :biomass_fresh_fruit_per_bunch, #average in kg
     :biomass_dry_fruit_per_bunch => (x -> fn_no_missings(x, mean)) => :biomass_dry_fruit_per_bunch, #average in kg
-    :n_of_bunch => (x -> fn_no_missings(x, mean)) => :total_n_bunches_harvested,
-    :n_of_fruit_total => (x -> fn_no_missings(x, mean)) => :total_n_fruit_harvested,
+    :n_of_bunch => (x -> fn_no_missings(x, median)) => :total_n_bunches_harvested,
+    :n_of_fruit_total => (x -> fn_no_missings(x, median)) => :total_n_fruit_harvested,
     :n_of_fruit_average => (x -> fn_no_missings(x, mean)) => :avg_n_fruit_per_bunch,
     :stalk_dry_biomass_per_bunch => (x -> fn_no_missings(x, mean)) => :stalk_dry_biomass_per_bunch, # in kg
     :stalk_fresh_biomass_per_bunch => (x -> fn_no_missings(x, mean)) => :stalk_fresh_biomass_per_bunch, # in kg
-    :bunch_water_content => (x -> fn_no_missings(x, mean)) => :bunch_water_content # in fraction
+    :bunch_water_content => (x -> fn_no_missings(x, mean)) => :bunch_water_content, # in fraction
+    :avg_stalk_water_content => (x -> fn_no_missings(x, mean)) => :stalk_water_content
 )
 
 df_CIGE_site = combine(groupby(df_CIGE, [:Site]), :bunch_fresh_mass_total => (x -> mean(filter(!ismissing, x) |> y -> filter(z -> z > 0.0, y))) => :avg_bunch_fresh_biomass)
